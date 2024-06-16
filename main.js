edee4cafeebc29df449cfc59ab6810d9b43173be
Mizanur Alrasid <88093931+mizanuralrasid@users.noreply.github.com>
2024-06-16 13:05:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// start screen
+let letsStart = document.getElementById("letsStart");
+letsStart.onclick = () => {
+    window.location.href = 'homeActivityChapters';
+};
+
+// home screen ----> chapters
+
+// for chapter1
+
+
+
