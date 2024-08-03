982aa25d1196f60fee0021e45f821097f2013661
Mizanur Alrasid <88093931+mizanuralrasid@users.noreply.github.com>
2024-08-03 22:16:35 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// start screen
+// let letsStart = document.getElementById("letsStart");
+// letsStart.onclick = function () {
+//     window.location.href = 'createAccount';
+// };
+
+
+let chapters = document.querySelectorAll(".chapters");
+
+chapters.forEach((chapter) => {
+    chapter.addEventListener("click",()=>{
+        console.log("chapter clicked");
+    })
+});
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,255 @@
+let chapter1 = document.querySelector(".chapter1");
+let ex1 = document.querySelector(".ex1");
+
+let chapter2 = document.querySelector(".chapter2");
+let ex2 = document.querySelector(".ex2");
+
+let chapter3 = document.querySelector(".chapter3");
+let ex3 = document.querySelector(".ex3");
+
+let chapter4 = document.querySelector(".chapter4");
+let ex4 = document.querySelector(".ex4");
+
+let chapter5 = document.querySelector(".chapter5");
+let ex5 = document.querySelector(".ex5");
+
+let chapter6 = document.querySelector(".chapter6");
+let ex6 = document.querySelector(".ex6");
+
+let chapter7 = document.querySelector(".chapter7");
+let ex7 = document.querySelector(".ex7");
+
+let chapter8 = document.querySelector(".chapter8");
+let ex8 = document.querySelector(".ex8");
+
+let chapter9 = document.querySelector(".chapter9");
+let ex9 = document.querySelector(".ex9");
+
+let chapter10 = document.querySelector(".chapter10");
+let ex10 = document.querySelector(".ex10");
+
+let chapter11 = document.querySelector(".chapter11");
+let ex11 = document.querySelector(".ex11");
+
+let chapter12 = document.querySelector(".chapter12");
+let ex12 = document.querySelector(".ex12");
+
+let chapter13 = document.querySelector(".chapter13");
+let ex13 = document.querySelector(".ex13");
+
+let chapter14 = document.querySelector(".chapter14");
+let ex14 = document.querySelector(".ex14");
+
+let chapter15 = document.querySelector(".chapter15");
+let ex15 = document.querySelector(".ex15");
+
+let chapter16 = document.querySelector(".chapter16");
+let ex16 = document.querySelector(".ex16");
+
+
+    chapter1.addEventListener("click",()=>{
+        let ex1 = document.querySelector(".ex1");
+        if (ex1.style.height === "0px") {
+            ex1.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex1.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter2.addEventListener("click",()=>{
+        let ex2 = document.querySelector(".ex2");
+        if (ex2.style.height === "0px") {
+            ex2.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex2.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter3.addEventListener("click",()=>{
+        let ex3 = document.querySelector(".ex3");
+        if (ex3.style.height === "0px") {
+            ex3.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex3.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter4.addEventListener("click",()=>{
+        let ex4 = document.querySelector(".ex4");
+        if (ex4.style.height === "0px") {
+            ex4.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex4.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter5.addEventListener("click",()=>{
+        let ex5 = document.querySelector(".ex5");
+        if (ex5.style.height === "0px") {
+            ex5.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex5.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter6.addEventListener("click",()=>{
+        let ex6 = document.querySelector(".ex6");
+        if (ex6.style.height === "0px") {
+            ex6.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex6.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter7.addEventListener("click",()=>{
+        let ex7 = document.querySelector(".ex7");
+        if (ex7.style.height === "0px") {
+            ex7.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex7.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter8.addEventListener("click",()=>{
+        let ex8 = document.querySelector(".ex8");
+        if (ex8.style.height === "0px") {
+            ex8.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex8.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter9.addEventListener("click",()=>{
+        let ex9 = document.querySelector(".ex9");
+        if (ex9.style.height === "0px") {
+            ex9.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex9.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter10.addEventListener("click",()=>{
+        let ex10 = document.querySelector(".ex10");
+        if (ex10.style.height === "0px") {
+            ex10.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex10.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter11.addEventListener("click",()=>{
+        let ex11 = document.querySelector(".ex11");
+        if (ex11.style.height === "0px") {
+            ex11.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex11.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter12.addEventListener("click",()=>{
+        let ex12 = document.querySelector(".ex12");
+        if (ex12.style.height === "0px") {
+            ex12.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex12.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter13.addEventListener("click",()=>{
+        let ex13 = document.querySelector(".ex13");
+        if (ex13.style.height === "0px") {
+            ex13.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex13.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter14.addEventListener("click",()=>{
+        let ex14 = document.querySelector(".ex14");
+        if (ex14.style.height === "0px") {
+            ex14.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex14.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter15.addEventListener("click",()=>{
+        let ex15 = document.querySelector(".ex15");
+        if (ex15.style.height === "0px") {
+            ex15.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex15.style.height = "0px";
+            console.log("close");
+        }
+    })
+
+
+    chapter16.addEventListener("click",()=>{
+        let ex16 = document.querySelector(".ex16");
+        if (ex16.style.height === "0px") {
+            ex16.style.height = "250px";
+            console.log("extend");
+        }
+        else{
+            ex16.style.height = "0px";
+            console.log("close");
+        }
+    })
