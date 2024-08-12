b99f5661c7ecf055aaf0c3e398ae938b7d913a4b
Mizanur Alrasid <88093931+mizanuralrasid@users.noreply.github.com>
2024-08-12 23:02:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,90 +49,105 @@ let ex16 = document.querySelector(".ex16");
 
     chapter1.addEventListener("click",()=>{
         let ex1 = document.querySelector(".ex1");
-        if (ex1.style.height === "0px") {
-            ex1.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex1.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex1.style.height === "0px") {
+        //     ex1.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex1.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter1/'
     })
 
 
     chapter2.addEventListener("click",()=>{
         let ex2 = document.querySelector(".ex2");
-        if (ex2.style.height === "0px") {
-            ex2.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex2.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex2.style.height === "0px") {
+        //     ex2.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex2.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter2/'
     })
 
 
     chapter3.addEventListener("click",()=>{
         let ex3 = document.querySelector(".ex3");
-        if (ex3.style.height === "0px") {
-            ex3.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex3.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex3.style.height === "0px") {
+        //     ex3.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex3.style.height = "0px";
+        //     console.log("close");
+        // }
+
+        window.location.href = 'chapters/chapter3/'
     })
 
 
     chapter4.addEventListener("click",()=>{
         let ex4 = document.querySelector(".ex4");
-        if (ex4.style.height === "0px") {
-            ex4.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex4.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex4.style.height === "0px") {
+        //     ex4.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex4.style.height = "0px";
+        //     console.log("close");
+        // }
+
+        window.location.href = 'chapters/chapter4/'
     })
 
 
     chapter5.addEventListener("click",()=>{
         let ex5 = document.querySelector(".ex5");
-        if (ex5.style.height === "0px") {
-            ex5.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex5.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex5.style.height === "0px") {
+        //     ex5.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex5.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter5/'
     })
 
 
     chapter6.addEventListener("click",()=>{
         let ex6 = document.querySelector(".ex6");
-        if (ex6.style.height === "0px") {
-            ex6.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex6.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex6.style.height === "0px") {
+        //     ex6.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex6.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter6/'
     })
 
 
     chapter7.addEventListener("click",()=>{
         let ex7 = document.querySelector(".ex7");
-        
+        // if (ex7.style.height === "0px") {
+        //     ex7.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex7.style.height = "0px";
+        //     console.log("close");
+        // }
         window.location.href = 'chapters/chapter7/'
     })
 
 
-        chapter8.addEventListener("click",()=>{
+    chapter8.addEventListener("click",()=>{
         let ex8 = document.querySelector(".ex8");
         // let chNo = document.getElementById("#chNo").value;
         // sessionStorage.setItem("chapterNo", chNo);
@@ -161,81 +176,88 @@ let ex16 = document.querySelector(".ex16");
         window.location.href = 'chapters/chapter9/index.html?9'
     })
 
+
     chapter10.addEventListener("click",()=>{
         let ex10 = document.querySelector(".ex10");
-        if (ex10.style.height === "0px") {
-            ex10.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex10.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex10.style.height === "0px") {
+        //     ex10.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex10.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter10/'
     })
 
 
     chapter11.addEventListener("click",()=>{
         let ex11 = document.querySelector(".ex11");
-        if (ex11.style.height === "0px") {
-            ex11.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex11.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex11.style.height === "0px") {
+        //     ex11.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex11.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter11/'
     })
 
 
     chapter12.addEventListener("click",()=>{
         let ex12 = document.querySelector(".ex12");
-        if (ex12.style.height === "0px") {
-            ex12.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex12.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex12.style.height === "0px") {
+        //     ex12.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex12.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter12/'
     })
 
 
     chapter13.addEventListener("click",()=>{
         let ex13 = document.querySelector(".ex13");
-        if (ex13.style.height === "0px") {
-            ex13.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex13.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex13.style.height === "0px") {
+        //     ex13.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex13.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter13/'
     })
 
 
     chapter14.addEventListener("click",()=>{
         let ex14 = document.querySelector(".ex14");
-        if (ex14.style.height === "0px") {
-            ex14.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex14.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex14.style.height === "0px") {
+        //     ex14.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex14.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter14/'
     })
 
 
     chapter15.addEventListener("click",()=>{
         let ex15 = document.querySelector(".ex15");
-        if (ex15.style.height === "0px") {
-            ex15.style.height = "250px";
-            console.log("extend");
-        }
-        else{
-            ex15.style.height = "0px";
-            console.log("close");
-        }
+        // if (ex15.style.height === "0px") {
+        //     ex15.style.height = "250px";
+        //     console.log("extend");
+        // }
+        // else{
+        //     ex15.style.height = "0px";
+        //     console.log("close");
+        // }
+        window.location.href = 'chapters/chapter15/'
     })
 
 
